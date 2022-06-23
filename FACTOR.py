44aee9af07162a88f7d42c19a518294a2d086980
Patrick Lodder <patricklodder@users.noreply.github.com>
2022-06-24 01:47:57 +0200

    use tx.txid instead of tx.hash
    The classically encoded transaction hash is stored in the txid
    field whereas the hash field contains the witness-encoded tx hash

--- FACTOR.py
+++ FACTOR.py
@@ -561,13 +561,13 @@ class CBlock(ctypes.Structure):
         # Update the coinbase transaction with the new extra nonce
         coinbase_script = coinbase_message 
         coinbase_tx['data'] = tx_make_coinbase(coinbase_script, scriptPubKey, block.blocktemplate['coinbasevalue'], block.blocktemplate['height'], block.blocktemplate['default_witness_commitment'])
-        coinbase_tx['hash'] = tx_compute_hash(coinbase_tx['data'])
+        coinbase_tx['txid'] = tx_compute_hash(coinbase_tx['data'])
 
         #Add transaction to our block
         block.blocktemplate['transactions'].insert(0, coinbase_tx)
         
         # Recompute the merkle root
-        block.blocktemplate['merkleroot'] = tx_compute_merkle_root([tx['hash'] for tx in block.blocktemplate['transactions']])   
+        block.blocktemplate['merkleroot'] = tx_compute_merkle_root([tx['txid'] for tx in block.blocktemplate['transactions']])
         merkleRoot = uint256()
         merkleRoot = (ctypes.c_uint64 * 4)(*hashToArray( block.blocktemplate["merkleroot"] if mine_latest_block else "6a3ae329ed61aee656ddbe14d8b2878125e96d0900ac0bb4c20c4f3300fb68d3")) 
         block.hashMerkleRoot = merkleRoot
