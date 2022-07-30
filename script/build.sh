4ff9106fb836ddc5321bb3b821b83935d3e3cd32
Escanor Liones <ReverseControl@users.noreply.github.com>
2022-07-30 00:08:13 -0400

    [build.sh] Update paths with the new folder structure.

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,5 @@
-g++ -fPIC src/gHash.cpp -shared -o gHash.so    -Wl,--whole-archive -lgmp -lcryptopp -Wl,--no-whole-archive
-g++ -fPIC src/gHash.cpp -shared -o libghash.so -Wl,--whole-archive -lgmp -lcryptopp -Wl,--no-whole-archive
-g++ -L.   src/gHash_test.cpp -l:libghash.so -o test_ghash
-LD_LIBRARY_PATH=.  ./test_ghash 
+mkdir -p libs
+g++ -fPIC ./src/gHash.cpp -shared -o ./libs/gHash.so    -Wl,--whole-archive -lgmp -lcryptopp -Wl,--no-whole-archive
+g++ -fPIC ./src/gHash.cpp -shared -o ./libs/libghash.so -Wl,--whole-archive -lgmp -lcryptopp -Wl,--no-whole-archive
+g++ -L.   ./src/gHash_test.cpp -l:./libs/libghash.so -o test_ghash
+LD_LIBRARY_PATH=./libs/  ./test_ghash 
